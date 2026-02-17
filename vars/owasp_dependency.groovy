52849821d2b206ff4f8da48bfe3e11d49171c60f
Anurag <anuragsharma921998@gmail.com>
2026-02-17 12:57:32 +0530

    Update owasp_dependency.groovy

--- vars/owasp_dependency.groovy
+++ vars/owasp_dependency.groovy
@@ -1,4 +1,7 @@
-def call(){
-  dependencyCheck additionalArguments: '--scan ./', odcInstallation: 'OWASP'
+def call(String nvdKey){
+  dependencyCheck additionalArguments: "--scan ./ --nvdApiKey ${nvdKey} --nvdDelay 15000 --data /var/jenkins_home/dc-data",
+  odcInstallation: 'OWASP'
+
   dependencyCheckPublisher pattern: '**/dependency-check-report.xml'
 }
+
